928542ca2e1bc50fa1ec9577b35f855942278e1a
aideremir <aideremir@bstd.ru>
2015-06-04 02:31:51 +0300

    backgorund plugin restored

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,13 +6,30 @@ module.exports = [
         "clobbers": [
             "window.PhoneCallTrap"
         ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "io.gvox.plugin.phonecalltrap": "0.1.0"
+    "io.gvox.plugin.phonecalltrap": "0.1.0",
+    "de.appplant.cordova.plugin.background-mode": "0.6.4",
+    "org.apache.cordova.device": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
