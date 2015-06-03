941d11b0dc3cd1b0c20f594bb7c90fd6b602d623
aideremir <aideremir@bstd.ru>
2015-06-04 02:27:27 +0300

    background plugin removed

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,25 +6,10 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
-        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
-        "clobbers": [
-            "cordova.plugins.backgroundMode",
-            "plugin.backgroundMode"
-        ]
-    },
-    {
         "file": "plugins/io.gvox.plugin.phonecalltrap/www/PhoneCallTrap.js",
         "id": "io.gvox.plugin.phonecalltrap.PhoneCallTrap",
         "clobbers": [
             "window.PhoneCallTrap"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
         ]
     }
 ];
@@ -32,9 +17,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "de.appplant.cordova.plugin.background-mode": "0.6.4",
-    "io.gvox.plugin.phonecalltrap": "0.1.0",
-    "org.apache.cordova.device": "0.3.0"
+    "io.gvox.plugin.phonecalltrap": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
